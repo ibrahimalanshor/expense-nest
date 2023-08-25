3b5d3b2084bd9b3ca7a39c5705c8a7c7752fce7e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-08-25 19:07:28 +0700

    feat: database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UsersModule, ConfigModule.forRoot({
-    isGlobal: true
-  })],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
